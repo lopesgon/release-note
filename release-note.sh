b3a64a6b6ae3773aa54af3b75b46f499fcf02eea
Frederic Lopes <mag.frederic@gmail.com>
2019-12-22 22:14:41 +0100

    Added comments in script

--- release-note.sh
+++ release-note.sh
@@ -1,14 +1,19 @@
 #!/bin/sh
 # By flopes
+# 1 - copy/paste script in project folder
+# 2 - update project (git rebase)
+# 3 - run script where first parameter is previous tag deployed
 
 FILENAME=release-note.txt
 
 if [ ! -n "$1" ]
 then
-	echo "$0 - Error! You must provide a start tag value not set or NULL"
+	echo "$0 - Error! You must provide a start tag value"
     exit
 else
 	echo "Starting Release Note Generator shell script..."
 fi
 
 git log $1..$2 --pretty=format:"* %s" > $FILENAME
+
+echo "... ending Release Note Generator shell script."
