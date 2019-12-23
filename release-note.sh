024ae6798ed2f36b9f5d9628f441dfcaa0f5bd9f
Frederic Lopes <mag.frederic@gmail.com>
2019-12-23 19:51:13 +0100

    Updated script to generate file with released version as filename

--- release-note.sh
+++ release-note.sh
@@ -1,19 +1,20 @@
 #!/bin/sh
 # By flopes
 # 1 - copy/paste script in project folder
-# 2 - update project (git rebase)
-# 3 - run script where first parameter is previous tag deployed
-
-FILENAME=release-note.txt
+# 2 - add to .gitignore: release-note_*
+# 3 - update project (git rebase)
+# 4 - run script where:
+# 		- first parameter is previous tag deployed
+#		- second parameter is last tag deployed
 
 if [ ! -n "$1" ]
 then
-	echo "$0 - Error! You must provide a start tag value"
+	echo "$0 - Error! You must provide first parameter as start tag value"
     exit
 else
 	echo "Starting Release Note Generator shell script..."
 fi
 
-git log $1..$2 --pretty=format:"* %s" > $FILENAME
+git log $1..$2 --pretty=format:"* %s" > release-note_$2.txt
 
 echo "... ending Release Note Generator shell script."
