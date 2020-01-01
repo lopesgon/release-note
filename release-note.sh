579037424fc404e6c73165ccfa56acbffb1430f5
Frederic Lopes <mag.frederic@gmail.com>
2020-01-01 19:49:35 +0100

    Fix how to

--- release-note.sh
+++ release-note.sh
@@ -2,10 +2,13 @@
 # By flopes
 
 # 1 - copy/paste script in project root folder
-# 2 - run: chmod 755 release-note.sh
-# 3 - add to .gitignore: release-note_*
+# 2 - run: 
+#		mkdir release-notes
+#		chmod 755 release-note.sh
+# 3 - add to .gitignore: 
+#		release-notes
 # 4 - update project (git rebase)
-# 5 - run: ./release-note.sh param1 param2 
+# 5 - run: ./release-note.sh param1 param2
 # 		- param1 is previous tag deployed
 #		- param2 is last tag deployed
 
@@ -17,6 +20,6 @@ else
 	echo "Starting Release Note Generator shell script..."
 fi
 
-git log $1..$2 --pretty=format:"* %s" > release-note_$2.txt
+git log $1..$2 --pretty=format:"* %s" > release-notes/release-note_$2.txt
 
 echo "... ending Release Note Generator shell script."
