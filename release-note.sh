b2c9baf2b64b7872ce8d8ced58b99aa2975814d5
Frederic Lopes <mag.frederic@gmail.com>
2020-01-01 19:45:49 +0100

    Updated how to of script for merge purpose

--- release-note.sh
+++ release-note.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 # By flopes
-# 1 - copy/paste script in project folder
-# 2 - add to .gitignore: release-note_*
-# 3 - update project (git rebase)
-# 4 - run script where:
-# 		- first parameter is previous tag deployed
-#		- second parameter is last tag deployed
+
+# 1 - copy/paste script in project root folder
+# 2 - run: chmod 755 release-note.sh
+# 3 - add to .gitignore: release-note_*
+# 4 - update project (git rebase)
+# 5 - run: ./release-note.sh param1 param2 
+# 		- param1 is previous tag deployed
+#		- param2 is last tag deployed
 
 if [ ! -n "$1" ]
 then
